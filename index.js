be8758bff40392153f9c8a9f064a1ae9ff66aa4e
Alf-Henning <alfhenning@gmail.com>
2022-10-24 12:44:42 +0200

    Setting up with railway

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const app = require('express')();
 const fetch = require('node-fetch');
 const PORT = 8080; 
 
-app.listen(
-    PORT, 
-    () => console.log('its alive')
-)
-
+app.listen(process.env.PORT || PORT, ()=> console.log("its running on port " + PORT))
 
 const powerPriceUrl = `https://playground-norway-power.ffail.win/?zone=NO1&date=2022-10-20&key=e17fd22c-487b-408f-8ab2-c3bed05a8da7`
 
@@ -16,7 +12,7 @@ fetch(powerPriceUrl)
     .then(response => response.json())
     .then(data => {
         const firstLine = data
-        console.log(firstLine);
+        
 
         app.get('/powerprice', (req, res) => {
             res.status(200).send({
