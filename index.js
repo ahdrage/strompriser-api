5e7b11fb98eb54c757a27b85f8a094694148ef1d
Alf-Henning <alfhenning@gmail.com>
2022-10-24 12:55:21 +0200

    Added hi script

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const app = require('express')(); 
 const fetch = require('node-fetch');
 const PORT = 8080; 
+const Koa = require('koa');
+const app = new Koa();
+
+
 
 app.listen(process.env.PORT || PORT, ()=> console.log("its running on port " + PORT))
+
+app.use(ctx => {
+    ctx.body = 'Hello Koa';
+  });
+
 
 const powerPriceUrl = `https://playground-norway-power.ffail.win/?zone=NO1&date=2022-10-20&key=e17fd22c-487b-408f-8ab2-c3bed05a8da7`
 
