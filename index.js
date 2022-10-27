28ef3ab6ea4f2fa127251d3335b700dae519e39e
Alf-Henning <alfhenning@gmail.com>
2022-10-27 21:07:16 +0200

    Getting today and tomorrows prices

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const app = require('express')(); 
+const app = require('express')();
 const fetch = require('node-fetch');
-const PORT = 8080; 
-require('dotenv').config(); 
+const PORT = 8080;
+require('dotenv').config();
 
-app.listen(process.env.PORT || PORT, ()=> console.log("its running on port " + PORT))
+app.listen(process.env.PORT || PORT, () => console.log("its running on port " + PORT))
 
 
 const keyPowerApi = process.env.KEY
@@ -12,18 +12,36 @@ const d = new Date();
 
 const currentHour = d.getHours()
 const dd = d.getDate();
-const mm = d.getMonth()+1;
-const yy = d.getFullYear(); 
+const mm = d.getMonth() + 1;
+const yy = d.getFullYear();
 
-todaysDate = yy+"-"+mm+"-"+dd
+todaysDate = yy + "-" + mm + "-" + dd
+
+const todayInMilliSeconds = new Date().getTime()
+const milliSecondsInOneDay = 24 * 60 * 60 * 1000
+const tomorrowInMilliseconds = todayInMilliSeconds + milliSecondsInOneDay
+const tomorrow = new Date(tomorrowInMilliseconds)
+const tomorrowDayOnly = tomorrow.getDate()
+const tomorrowMonthOnly = tomorrow.getMonth() + 1;
+const tomorrowYearOnly = tomorrow.getFullYear();
+const tomorrowsDate = tomorrowYearOnly + "-" + tomorrowMonthOnly + "-" + tomorrowDayOnly
 
 console.log(todaysDate);
+console.log(tomorrowsDate);
 
 
-// const powerPriceUrl = `https://playground-norway-power.ffail.win/?zone=NO1&date=${todaysDate}&key=${keyPowerApi}`
-const powerPriceUrl = `https://norway-power.ffail.win?zone=NO1&date=${todaysDate}&key=${keyPowerApi}` 
 
-fetch(powerPriceUrl)
+// prod
+const powerPriceUrlToday = `https://norway-power.ffail.win?zone=NO1&date=${todaysDate}&key=${keyPowerApi}` 
+const powerPriceUrlTomorrow = `https://norway-power.ffail.win?zone=NO1&date=${tomorrowsDate}&key=${keyPowerApi}` 
+
+// test
+// const powerPriceUrlToday = `https://playground-norway-power.ffail.win/?zone=NO1&date=${todaysDate}&key=${keyPowerApi}`
+// const powerPriceUrlTomrrow = `https://playground-norway-power.ffail.win/?zone=NO1&date=${tomorrowsDate}&key=${keyPowerApi}`
+
+
+
+fetch(powerPriceUrlToday)
     .then(response => response.json())
     .then(data => {
         const firstLine = data
@@ -37,4 +55,18 @@ fetch(powerPriceUrl)
 
     })
 
+
+fetch(powerPriceUrlTomrrow)
+    .then(response => response.json())
+    .then(data => {
+        const firstLine = data
+
+        app.get('/powerpriceTomorrow', (req, res) => {
+            res.set('Access-Control-Allow-Origin', '*');
+            res.status(200).send({
+                firstLine
+            })
+        })
+
+    })
 
