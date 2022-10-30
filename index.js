1971b295b4e7d9d86c6ecd81896c557988552a31
Alf-Henning <alfhenning@gmail.com>
2022-10-30 22:19:34 +0100

    Added new API endpoints

--- index.js
+++ index.js
@@ -42,15 +42,23 @@ console.log(tomorrowsDate);
 // const powerPriceUrlToday = `https://playground-norway-power.ffail.win/?zone=NO1&date=${todaysDate}&key=${keyPowerApi}`
 // const powerPriceUrlTomorrow = `https://playground-norway-power.ffail.win/?zone=NO1&date=${tomorrowsDate}&key=${keyPowerApi}`
 
-
+// today
 const powerPriceUrlTodayZone1 = 'https://strompriser-base-api-production.up.railway.app/powerprice-zone1'
 const powerPriceUrlTodayZone2 = 'https://strompriser-base-api-production.up.railway.app/powerprice-zone2'
 const powerPriceUrlTodayZone3 = 'https://strompriser-base-api-production.up.railway.app/powerprice-zone3'
 const powerPriceUrlTodayZone4 = 'https://strompriser-base-api-production.up.railway.app/powerprice-zone4'
 const powerPriceUrlTodayZone5 = 'https://strompriser-base-api-production.up.railway.app/powerprice-zone5'
 
- 
+// tomorrow
+const powerPriceUrlTomorrowZone1 = 'https://strompriser-base-api-production.up.railway.app/powerprice-zone1-tomorrow'
+const powerPriceUrlTomorrowZone2 = 'https://strompriser-base-api-production.up.railway.app/powerprice-zone2-tomorrow'
+const powerPriceUrlTomorrowZone3 = 'https://strompriser-base-api-production.up.railway.app/powerprice-zone3-tomorrow'
+const powerPriceUrlTomorrowZone4 = 'https://strompriser-base-api-production.up.railway.app/powerprice-zone4-tomorrow'
+const powerPriceUrlTomorrowZone5 = 'https://strompriser-base-api-production.up.railway.app/powerprice-zone5-tomorrow'
 
+
+ 
+const job = schedule.scheduleJob('02 23 * * *', function () {
     fetch(powerPriceUrlTodayZone1)
         .then(response => response.json())
         .then(data => {
@@ -60,7 +68,7 @@ const powerPriceUrlTodayZone5 = 'https://strompriser-base-api-production.up.rail
 
 
 
-            app.get('/powerprice-zone1', (req, res) => {
+            app.get('/powerprice-today-zone1', (req, res) => {
                 res.set('Access-Control-Allow-Origin', '*');
                 res.status(200).send({
                     firstLine
@@ -70,21 +78,24 @@ const powerPriceUrlTodayZone5 = 'https://strompriser-base-api-production.up.rail
         })
 
     
+    })
   
     
-/* 
-    fetch(powerPriceUrlTomorrow)
+    const job = schedule.scheduleJob('02 13 * * *', function () {
+
+
+    fetch(powerPriceUrlTomorrowZone1)
         .then(response => response.json())
         .then(data => {
             const firstLine = data
 
-            app.get('/powerpriceTomorrow', (req, res) => {
+            app.get('/powerprice-tomorrow-zone1', (req, res) => {
                 res.set('Access-Control-Allow-Origin', '*');
                 res.status(200).send({
                     firstLine
                 })
             })
 
-        }) */
+        }) 
 
-  
+    })
